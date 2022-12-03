5fa486a2408ddc088685dd84d40c047953cd1e23
barcelona2004 <barcelona200418@yandex.ru>
2022-12-03 22:28:51 +0700

    fixed step 9

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -11,20 +11,28 @@ def main():
     name = prompt.string('May I have your name? ')
     print("Hello, " + name + "!")
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    x = randint(1, 1000)
-    print(f"Question: {x}")
-    answer = input()
-    print(f"Your answer: {answer}")
-    if answer == 'yes' or answer == 'no':
-        if is_prime(x) and answer == 'yes':
-            print("Correct!")
-        elif not is_prime(x) and answer == 'no':
-            print("Correct!")
-        elif is_prime(x) and answer == 'no':
-            print("'no' is wrong answer ;(. Correct answer was 'yes'.")
-            print(f"Let's try again {name}")
+    count = 0
+    for _ in range(3):
+        x = randint(1, 1000)
+        print(f"Question: {x}")
+        answer = input()
+        print(f"Your answer: {answer}")
+        if answer == 'yes' or answer == 'no':
+            if is_prime(x) and answer == 'yes':
+                print("Correct!")
+            elif not is_prime(x) and answer == 'no':
+                print("Correct!")
+            elif is_prime(x) and answer == 'no':
+                print("'no' is wrong answer ;(. Correct answer was 'yes'.")
+                print(f"Let's try again {name}")
+                break
+            else:
+                print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+                print(f"Let's try again {name}")
+                break
         else:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
             print(f"Let's try again {name}")
-    else:
-        print(f"Let's try again {name}")
+            break
+        count += 1
+    if count == 3:
+        print(f"Congratulations, {name}!")
