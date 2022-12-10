b3552241b36b03ebd9bc5415c3d339e1db035e55
barcelona2004 <barcelona200418@yandex.ru>
2022-12-10 19:44:44 +0700

    fix brain-gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -2,6 +2,13 @@ from brain_games.games.brain_games import hello, try_again, win
 from random import randint
 
 
+def gcd(number1, number2):
+    max_num = 0
+    for i in range(1, min(number1, number2) + 1):
+        if number2 % i == 0 and number1 % i == 0:
+            max_num = max(max_num, i)
+    return max_num
+
 def main():
     hello()
     print("Find the greatest common divisor of given numbers.")
@@ -12,10 +19,7 @@ def main():
         print(f"Question: {number1} {number2}")
         answer = input()
         print(f"Your answer: {answer}")
-        max_num = 0
-        for i in range(1, min(number1, number2) + 1):
-            if number2 % i == 0 and number1 % i == 0:
-                max_num = max(max_num, i)
+        max_num = gcd(number1, number2)
         if max_num == int(answer):
             print("Correct!")
             count += 1
