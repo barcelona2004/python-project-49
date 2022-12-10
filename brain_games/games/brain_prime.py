73fa99b0b036af60898fc390993f3bfc33261add
barcelona2004 <barcelona200418@yandex.ru>
2022-12-10 19:15:01 +0700

    fix brain_prime

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -2,8 +2,21 @@ from brain_games.games.brain_games import hello, try_again, win
 from random import randint
 
 
-def is_prime(x):
-    return x > 1 and all(x % i != 0 for i in range(2, int(x ** 0.5) + 1))
+def is_prime(number):
+    for i in range(2, round(number ** 0.5) + 1):
+        if number % i == 0:
+            return 'no'
+    return 'yes'
+
+
+def is_correct_answer(answer, correct_answer):
+    if correct_answer == answer:
+        print("Correct!")
+    else:
+        print(f"'{answer}' is wrong answer ;(."
+              f"Correct answer was '{correct_answer}'.")
+        try_again()
+        exit(0)
 
 
 def main():
@@ -11,27 +24,16 @@ def main():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     count = 0
     for _ in range(3):
-        x = randint(1, 1000)
-        print(f"Question: {x}")
+        random_number = randint(1, 1000)
+        correct_answer = is_prime(random_number)
+        print(f"Question: {random_number}")
         answer = input()
         print(f"Your answer: {answer}")
-        if answer == 'yes' or answer == 'no':
-            if (is_prime(x) and answer == 'yes') or \
-                    (not is_prime(x) and answer == 'no'):
-                print("Correct!")
-            elif is_prime(x) and answer == 'no':
-                print("'no' is wrong answer ;(."
-                      "Correct answer was 'yes'.")
-                try_again()
-                break
-            else:
-                print("'yes' is wrong answer ;(."
-                      "Correct answer was 'no'.")
-                try_again()
-                break
-        else:
+        if answer != 'yes' and answer != 'no':
             try_again()
-            break
+            return 0
+        else:
+            is_correct_answer(answer, correct_answer)
         count += 1
     if count == 3:
         win()
