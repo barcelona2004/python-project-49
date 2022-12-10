fe62cd4795b0e05a8d388b58c216de8cef91acad
barcelona2004 <barcelona200418@yandex.ru>
2022-12-10 18:39:58 +0700

    final test for codeclimate
    ''

--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -2,16 +2,20 @@ from brain_games.games.brain_games import hello, try_again, win
 from random import randint
 
 
-def is_even(random_number, answer):
+def is_correct_answer(random_number):
+    if random_number % 2 == 0:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def is_even(random_number, answer, correct_answer):
     if (random_number % 2 == 0 and answer == 'yes') or \
             (random_number % 2 != 0 and answer == 'no'):
         print("Correct!")
-    elif random_number % 2 == 0 and answer == 'no':
-        print("'no' is wrong answer ;(. Correct answer was 'yes'.")
-        try_again()
-        exit(0)
     else:
-        print("'yes' is wrong answer ;(. Correct answer was 'no'.")
+        print(f"'{answer}' is wrong answer ;(."
+              f"Correct answer was '{correct_answer}'.")
         try_again()
         exit(0)
 
@@ -24,12 +28,13 @@ def main():
         random_number = randint(1, 100)
         print("Question: " + str(random_number))
         answer = input()
+        correct_answer = is_correct_answer(random_number)
         print("Your answer: " + answer)
         if answer != 'yes' and answer != 'no':
             try_again()
             return 0
         else:
-            is_even(random_number, answer)
+            is_even(random_number, answer, correct_answer)
         count += 1
     if count == 3:
         win()
