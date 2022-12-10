3f50b8e2d9a2f5e18e46f28539aac73ac98d38bd
barcelona2004 <barcelona200418@yandex.ru>
2022-12-10 20:20:00 +0700

    fix gcd and calc

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,5 +1,37 @@
-from brain_games.games.brain_games import hello, try_again, win
+#from brain_games.games.brain_games import hello, try_again, win
 from random import randint
+from brain_games import *
+
+
+def calc_sum(first_sum, second_sum, summary):
+    if first_sum + second_sum == int(summary):
+        print("Correct!")
+    else:
+        print(f"'{summary}' is wrong answer ;(."
+              f"Correct answer was '{second_sum + first_sum}'.")
+        try_again()
+        exit(0)
+
+
+def calc_diff(first_dif, second_dif, differ):
+    if first_dif - second_dif == int(differ):
+        print("Correct!")
+    else:
+        print(f"'{differ}' is wrong answer ;(."
+              f"Correct answer was '{first_dif - second_dif}'.")
+        try_again()
+        exit(0)
+
+
+def calc_time(first_tim, second_tim, time):
+    if first_tim * second_tim == int(time):
+        print("Correct!")
+        win()
+    else:
+        print(f"'{time}' is wrong answer ;(."
+              f"Correct answer was '{first_tim * second_tim}'.")
+        try_again()
+
 
 
 def main():
@@ -14,32 +46,15 @@ def main():
     print(f"Question: {first_sum} + {second_sum}")
     summary = input()
     print(f"Your answer: {summary}")
-    if first_sum + second_sum == int(summary):
-        print("Correct!")
-        print(f"Question: {first_dif} - {second_dif}")
-        differ = input()
-        print(f"Your answer: {differ}")
-        if first_dif - second_dif == int(differ):
-
-            print("Correct!")
-            print(f"Question: {first_tim} * {second_tim}")
-            time = input()
-            print(f"Your answer: {time}")
-            if first_tim * second_tim == int(time):
-                print("Correct!")
-                win()
-            else:
-                print(f"'{time}' is wrong answer ;(."
-                      f"Correct answer was '{first_tim * second_tim}'.")
-                try_again()
-        else:
-            print(f"'{differ}' is wrong answer ;(."
-                  f"Correct answer was '{first_dif - second_dif}'.")
-            try_again()
-    else:
-        print(f"'{summary}' is wrong answer ;(."
-              f"Correct answer was '{second_sum + first_sum}'.")
-        try_again()
+    calc_sum(first_sum, second_sum, summary) 
+    print(f"Question: {first_dif} - {second_dif}")
+    differ = input()
+    print(f"Your answer: {differ}")
+    calc_diff(first_dif, second_dif, differ)
+    print(f"Question: {first_tim} * {second_tim}")
+    time = input()
+    print(f"Your answer: {time}")
+    calc_time(first_tim, second_tim, time)
 
 
 if __name__ == '__main__':
