57e2dba0db9a24c9fee3edb899f2c3d9a62247bd
barcelona2004 <barcelona200418@yandex.ru>
2022-12-14 14:57:14 +0700

    Fix brain-progression logic

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -8,6 +8,7 @@ def build_progression(d, num):
         if k == num:
             lst.append('..')
             x = k - 1
+            result = i
         else:
             lst.append(str(i))
         k += 1
@@ -15,13 +16,12 @@ def build_progression(d, num):
     for j in lst:
         line += j
         line += ' '
-    return line, lst, x
+    return line, lst, x, result
 
 
 def get_game_progression():
     d = randint(1, 10)
     num = randint(1, 10)
-    line, lst, x = build_progression(d, num)
-    result = int(lst[x]) + d
+    line, lst, x, result = build_progression(d, num)
     line = f"Question: {line}"
     return line, result
